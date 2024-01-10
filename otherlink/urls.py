e850b86cbf7884a14e7ff40bc7a6c6a117877873
unwoo <sincerity1129@naver.com>
2024-01-10 23:23:49 +0900

    otheruser html & content delete fixed

--- otherlink/urls.py
+++ otherlink/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'otheruser_app'
 
 urlpatterns = [
-    re_path('', OtherlinkProcess.as_view(), name="otheruser_home"),
+    re_path('content', OtherlinkProcess.as_view(), name="otheruser_home"),
     ]
 
 # 설정하고자 하는 url은 GET 방식으로 받는 html route 경로를 따라야함
